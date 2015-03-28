5810214b89172807901d6a263c76771268b17bd0
Anders Myren <esnalabu@hotmail.com>
2015-03-28 12:41:50 +0100

    Added check for symlink to ensure symlink is created

--- backup.sh
+++ backup.sh
@@ -15,7 +15,11 @@ mount -o rw,remount $PATHTOBACKUP
 rsync -aAXvz --exclude-from $EXCLUDEFILE --link-dest=$PATHTOBACKUP/current $SOURCE $PATHTOBACKUP/back-$date
 
 # Make new symlink for current
-rm $PATHTOBACKUP/current && ln -s back-$date $PATHTOBACKUP/current
+if [ -e $PATHTOBACKUP/current ]; then
+    rm $PATHTOBACKUP/current && ln -s back-$date $PATHTOBACKUP/current
+else
+    ln -s back-$date $PATHTOBACKUP/current
+fi
 
 # Write protect disk
 mount -o ro,remount $PATHTOBACKUP
